057b075e57233b12aff94e6b5b36283de0d25642
MiracleUFO <miracle.ufodiama.242949@unn.edu.ng>
2022-11-14 00:33:31 +0100

    feat: places page and filter tab

--- src/pages/Places/index.js
+++ src/pages/Places/index.js
@@ -1,7 +1,32 @@
+import { useState } from 'react';
+import PlacesGrid from '../../containers/PlacesGrid';
+import FilterIcon from '../../assets/filter-icon.svg';
 import styles from './styles.module.css';
 
 const Places = () => {
-  return (<main className={styles.main}>Places</main>)
+  const [showTextContainer, setShowTextContainer] = useState(false);
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.filter_container}>
+        <div className={`${styles.text_container} ${showTextContainer ? styles.show : ''}`}>
+          <p>Resturant</p>
+          <p>Cottage</p>
+          <p>Castle</p>
+          <p>fantasy city</p>
+          <p>beach</p>
+          <p>Cabins</p>
+          <p>Off-grid</p>
+          <p>Farm</p>
+        </div>
+        <button className={styles.filter_icon} onClick={() => setShowTextContainer(!showTextContainer)}>
+          <p>Location</p>
+          <img src={FilterIcon} alt="Filter icon" />
+        </button>
+      </div>
+      <PlacesGrid />
+    </main>
+  );
 };
 
 export default Places;
