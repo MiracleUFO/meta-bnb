3858204030ba9a7675f01327c86ed27fd0045601
MiracleUFO <miracle.ufodiama.242949@unn.edu.ng>
2022-11-13 13:38:29 +0100

    chore: routing setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Places from './pages/Places';
+import NotFound from './pages/404';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/places" element={<Places />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
