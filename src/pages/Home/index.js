1b7bf75dddc75e38cc8843bed7c34f77ad22e4f6
MiracleUFO <miracle.ufodiama.242949@unn.edu.ng>
2022-11-15 02:00:27 +0100

    feat: home page hero section done, image grid in hero

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,10 @@
 import Banner from '../../components/Banner';
 import SearchBar from '../../components/SearchBar';
 import PlacesGrid from '../../containers/PlacesGrid';
+
+import { images } from '../../containers/PlacesGrid/constants';
+import GridImageOne from '../../assets/grid-img15-ortho.png';
+
 import styles from './styles.module.css';
 
 const Home = () => {
@@ -17,6 +21,17 @@ const Home = () => {
           </p>
           <SearchBar />
         </article>
+
+        <div className={styles.img_grid}>
+          <div>
+            <img src={GridImageOne} alt="House 1" />
+            <img src={images[15]} alt="House 2" />
+          </div>
+          <div>
+            <img src={images[8]} alt="House 3" />
+            <img src={images[9]} alt="House 2" />
+          </div>
+        </div>
       </section>
 
       <Banner />
